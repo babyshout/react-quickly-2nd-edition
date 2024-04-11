53723de9cac513b618dae86910997ced1b99a1c9
babyshout <lsms0523@gmail.com>
2024-04-11 21:34:22 +0900

    24-04-11 react quickly 2nd edition

--- chapter04-functional_component/rq04-gallery-function-v2/src/App.js
+++ chapter04-functional_component/rq04-gallery-function-v2/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
@@ -9,30 +9,26 @@ function App() {
   );
 }
 
-const Gallery = () => 
- (
-    <section style={{display: "flex"}}>
-      <Image index="1003" title='Deer' />
-      <Image index='1020' title='Bear' />
-      <Image index='1024' title='Vulture' />
-      <Image index='1084' title='Walrus' />
-    </section>
-  );
+const Gallery = () => (
+  <section style={{ display: "flex" }}>
+    <Image index="1003" title="Deer" />
+    <Image index="1020" title="Bear" />
+    <Image index="1024" title="Vulture" />
+    <Image index="1084" title="Walrus" />
+  </section>
+);
 
-const Image = ({index, title}) => {
+const getImageSource = (index) => `//picsum.photos/200/100/animals/${index}/`;
 
+const Image = ({ index, title }) => {
   return (
-    <figure style={{margin: '5px'}}>
-      <img
-      src={`//picsum.photos/id/${index}/150/150/`}
-      alt={title}
-      />
+    <figure style={{ margin: "5px" }}>
+      <img src={getImageSource(index)} alt={title} />
       <figcaption>
         <h3>Species: {title}</h3>
       </figcaption>
     </figure>
-  )
-
-}
+  );
+};
 
 export default App;
